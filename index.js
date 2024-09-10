8703a00ee8119702e28a118acfd74faacb1310db
PrincegdsMD <158026058+PrincegdsMD@users.noreply.github.com>
2024-09-10 15:39:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,117 +1,108 @@
-import { Boom } from '@hapi/boom'
+import { Boom } from '@hapi/boom';
 import Baileys, {
   DisconnectReason,
   delay,
-  Browsers,
   useMultiFileAuthState
-} from '@whiskeysockets/baileys'
-import cors from 'cors'
-import express from 'express'
-import fs from 'fs'
-import { readFile } from 'fs/promises'
+} from '@whiskeysockets/baileys';
+import cors from 'cors';
+import express from 'express';
+import fs from 'fs';
+import { readFile } from 'fs/promises';
 import { Pastebin, PrivacyLevel, ExpirationTime } from "pastedeno";
-import path, { dirname } from 'path'
-import pino from 'pino'
-import { fileURLToPath } from 'url'
+import path, { dirname } from 'path';
+import pino from 'pino';
+import { fileURLToPath } from 'url';
 
-const app = express()
+const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
 
-  res.setHeader('Pragma', 'no-cache')
-
-  res.setHeader('Expires', '0')
-  next()
-})
-
-app.use(cors())
+app.use(cors());
 
 const pastebin = new Pastebin({
   api_dev_key: "06S06TKqc-rMUHoHsrYxA_bwWp9Oo12y",
 });
 
-let PORT = process.env.PORT || 8000
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+let PORT = process.env.PORT || 8000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 function createRandomId() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  let id = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let id = '';
   for (let i = 0; i < 10; i++) {
-    id += characters.charAt(Math.floor(Math.random() * characters.length))
+    id += characters.charAt(Math.floor(Math.random() * characters.length));
   }
-  return id
+  return id;
 }
 
-let sessionFolder = `./auth/${createRandomId()}`
+let sessionFolder = `./auth/${createRandomId()}`;
 if (fs.existsSync(sessionFolder)) {
   try {
-    fs.rmdirSync(sessionFolder, { recursive: true })
-    console.log('Deleted the "SESSION" folder.')
+    fs.rmdirSync(sessionFolder, { recursive: true });
+    console.log('Deleted the "SESSION" folder.');
   } catch (err) {
-    console.error('Error deleting the "SESSION" folder:', err)
+    console.error('Error deleting the "SESSION" folder:', err);
   }
 }
 
 let clearState = () => {
-  fs.rmdirSync(sessionFolder, { recursive: true })
-}
+  fs.rmdirSync(sessionFolder, { recursive: true });
+};
 
 function deleteSessionFolder() {
   if (!fs.existsSync(sessionFolder)) {
-    console.log('The "SESSION" folder does not exist.')
-    return
+    console.log('The "SESSION" folder does not exist.');
+    return;
   }
 
   try {
-    fs.rmdirSync(sessionFolder, { recursive: true })
-    console.log('Deleted the "SESSION" folder.')
+    fs.rmdirSync(sessionFolder, { recursive: true });
+    console.log('Deleted the "SESSION" folder.');
   } catch (err) {
-    console.error('Error deleting the "SESSION" folder:', err)
+    console.error('Error deleting the "SESSION" folder:', err);
   }
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get('/', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-/* app.get('/', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
-
 app.get('/qr', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'qr.html'))
-})
+  res.sendFile(path.join(__dirname, 'qr.html'));
+});
 
 app.get('/code', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-}); */
+  res.sendFile(path.join(__dirname, 'pair.html'));
+});
 
 app.get('/pair', async (req, res) => {
-  let phone = req.query.phone
+  let phone = req.query.phone;
 
-  if (!phone) return res.json({ error: 'Please Provide Phone Number' })
+  if (!phone) return res.json({ error: 'Please Provide Phone Number' });
 
   try {
-    const code = await startnigg(phone)
-    res.json({ code: code })
+    const code = await startnigg(phone);
+    res.json({ code: code });
   } catch (error) {
-    console.error('Error in WhatsApp authentication:', error)
-    res.status(500).json({ error: 'Internal Server Error' })
+    console.error('Error in WhatsApp authentication:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-})
+});
 
 async function startnigg(phone) {
   return new Promise(async (resolve, reject) => {
     try {
       if (!fs.existsSync(sessionFolder)) {
-        await fs.mkdirSync(sessionFolder)
+        await fs.mkdirSync(sessionFolder);
       }
 
-      const { state, saveCreds } = await useMultiFileAuthState(sessionFolder)
+      const { state, saveCreds } = await useMultiFileAuthState(sessionFolder);
 
       const negga = Baileys.makeWASocket({
         version: [2, 3000, 1015901307],
@@ -119,111 +110,118 @@ async function startnigg(phone) {
         logger: pino({
           level: 'silent',
         }),
-        browser: Browsers.ubuntu("Chrome"),
+        browser: ['Ubuntu', 'Chrome', '20.0.04'],
         auth: state,
-      })
+      });
 
       if (!negga.authState.creds.registered) {
-        let phoneNumber = phone ? phone.replace(/[^0-9]/g, '') : ''
+        let phoneNumber = phone ? phone.replace(/[^0-9]/g, '') : '';
         if (phoneNumber.length < 11) {
-          return reject(new Error('Please Enter Your Number With Country Code !!'))
+          return reject(new Error('Please Enter Your Number With Country Code !!'));
         }
         setTimeout(async () => {
           try {
-            let code = await negga.requestPairingCode(phoneNumber)
-            console.log(`Your Pairing Code : ${code}`)
-            resolve(code)
+            let code = await negga.requestPairingCode(phoneNumber);
+            console.log(`Your Pairing Code : ${code}`);
+            resolve(code);
           } catch (requestPairingCodeError) {
-            const errorMessage = 'Error requesting pairing code from WhatsApp'
-            console.error(errorMessage, requestPairingCodeError)
-            return reject(new Error(errorMessage))
+            const errorMessage = 'Error requesting pairing code from WhatsApp';
+            console.error(errorMessage, requestPairingCodeError);
+            return reject(new Error(errorMessage));
           }
-        }, 3000)
+        }, 3000);
       }
 
-      negga.ev.on('creds.update', saveCreds)
+      negga.ev.on('creds.update', saveCreds);
 
       negga.ev.on('connection.update', async update => {
-        const { connection, lastDisconnect } = update
+        const { connection, lastDisconnect } = update;
 
         if (connection === 'open') {
-          await delay(10000)
-          let data1 = await readFile(`${sessionFolder}/creds.json`)
+          await delay(10000);
+          let data1 = await readFile(`${sessionFolder}/creds.json`);
           const output = await pastebin.createPaste({
             text: data1.toString(),
-            title: "PrincdMd",
+            title: "PrinceMd",
             format: "javascript",
             privacy: PrivacyLevel.UNLISTED,
             expiration: ExpirationTime.ONE_MONTH
-        });
-          
-          const sessi = 'PrinceBot~' + output.split('https://pastebin.com/')[1]
-          console.log(sessi)
-          await delay(2000)
-          let guru = await negga.sendMessage(negga.user.id, { text: sessi })
-          await delay(2000)
+          });
+
+          const sessi = 'PrinceBot~' + output.split('https://pastebin.com/')[1];
+          console.log(sessi);
+          await delay(2000);
+          let guru = await negga.sendMessage(negga.user.id, { text: sessi });
+          await delay(2000);
           await negga.groupAcceptInvite("Jo5bmHMAlZpEIp75mKbwxP");
-          await delay(1000)
+          await delay(2000);
           await negga.sendMessage(
             negga.user.id,
             {
               text: 'Hello there!👋🏻 \n\nDo not share your session id with anyone.\n\nPut the above in SESSION_ID var\n\nThanks for using PRINCE-BOT\n\njoin support Channel:- https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP\n\nDont forget to give star 🌟 to Prince bot repo\nhttps://github.com/PRINCE-GDS/PRINXE-MD\n',
             },
             { quoted: guru }
-          )
+          );
 
-          console.log('Connected to WhatsApp Servers')
+          console.log('Connected to WhatsApp Servers');
 
           try {
-            deleteSessionFolder()
+            deleteSessionFolder();
           } catch (error) {
-            console.error('Error deleting session folder:', error)
+            console.error('Error deleting session folder:', error);
           }
 
-          process.send('reset')
+          process.send('reset');
         }
 
         if (connection === 'close') {
-          let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-          console.log('Connection Closed:', reason)
-          if (reason === DisconnectReason.connectionClosed) {
-            console.log('[Connection closed, reconnecting....!]')
-            process.send('reset')
-          } else if (reason === DisconnectReason.connectionLost) {
-            console.log('[Connection Lost from Server, reconnecting....!]')
-            process.send('reset')
-          } else if (reason === DisconnectReason.loggedOut) {
-            clearState()
-            console.log('[Device Logged Out, Please Try to Login Again....!]')
-            process.send('reset')
-          } else if (reason === DisconnectReason.restartRequired) {
-            console.log('[Server Restarting....!]')
-            startnigg()
-          } else if (reason === DisconnectReason.timedOut) {
-            console.log('[Connection Timed Out, Trying to Reconnect....!]')
-            process.send('reset')
-          } else if (reason === DisconnectReason.badSession) {
-            console.log('[BadSession exists, Trying to Reconnect....!]')
-            clearState()
-            process.send('reset')
-          } else if (reason === DisconnectReason.connectionReplaced) {
-            console.log(`[Connection Replaced, Trying to Reconnect....!]`)
-            process.send('reset')
-          } else {
-            console.log('[Server Disconnected: Maybe Your WhatsApp Account got Fucked....!]')
-            process.send('reset')
-          }
+          let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+          console.log('Connection Closed:', reason);
+          await handleDisconnection(reason);
         }
-      })
+      });
 
-      negga.ev.on('messages.upsert', () => {})
+      negga.ev.on('messages.upsert', () => {});
     } catch (error) {
-      console.error('An Error Occurred:', error)
-      throw new Error('An Error Occurred')
+      console.error('An Error Occurred:', error);
+      throw new Error('An Error Occurred');
     }
-  })
+  });
+}
+
+async function handleDisconnection(reason) {
+  switch (reason) {
+    case DisconnectReason.connectionClosed:
+    case DisconnectReason.connectionLost:
+    case DisconnectReason.timedOut:
+    case DisconnectReason.restartRequired:
+      console.log('[Reconnecting...]');
+      await reconnect();
+      break;
+    case DisconnectReason.badSession:
+      clearState();
+      console.log('[Bad Session, Reconnecting...]');
+      await reconnect();
+      break;
+    case DisconnectReason.connectionReplaced:
+      console.log('[Connection Replaced, Reconnecting...]');
+      await reconnect();
+      break;
+    default:
+      console.log('[Disconnected: Unknown reason]');
+      break;
+  }
+}
+
+async function reconnect() {
+  try {
+    await delay(5000); // Wait before reconnecting
+    await startnigg(); // Call your start function again
+  } catch (error) {
+    console.error('Reconnection failed:', error);
+  }
 }
 
 app.listen(PORT, () => {
-  console.log(`API Running on PORT:${PORT}`)
-})
+  console.log(`API Running on PORT:${PORT}`);
+});
