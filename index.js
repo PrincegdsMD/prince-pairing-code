c7ba8067a8a8f48473e20b104d56d18d891dfdfd
Princeshoro <115966212+Princeshoro@users.noreply.github.com>
2024-09-10 15:16:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function deleteSessionFolder() {
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 /* app.get('/', async (req, res) => {
